316bfe3f53c59fcaf89986857f52be110daf447b
aechpeychay <aech@example.com>
2024-03-24 23:23:42 +0300

    fourth

---
+++ lab2_2.py
@@ -0,0 +1,9 @@
+def picks():
+	x_arr = [1,1,1,1]
+	sig1 = (((5)^0.5 + 3)/4*2^0.5)*a
+	sig1 = (((5)^0.5 - 1)/4*2^0.5)*a
+	for i in 4:
+		if x_arr[i] = x_arr[i] + sig1 
+x0 = input()
+a = input()
+x_arr = []
---
+++ lab2_3.py
@@ -0,0 +1,9 @@
+def f():
+	
+a = input()#1
+b = input()#0.5
+y = input()#2
+xh = input()
+xg = input()
+xz = input()
+eps = 0.0000001
